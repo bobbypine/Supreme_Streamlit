0ecba859884974f9000d3855b8c4b46261bc5d07
Rob <rob.rudary@gmail.com>
2021-12-17 09:32:18 -0500

    Prepping for launch

---
+++ App.py
@@ -0,0 +1,37 @@
+import Keyword
+import streamlit as st
+from urllib.error import URLError
+import time
+import sys
+
+st.image('Supreme_Logo.png')
+
+try:
+
+    userkey = st.text_input(label='Enter Your Keyword')
+
+except URLError as e:
+    st.error(f'This Demo Requires Internet Access: {e.reason}')
+
+
+if __name__ == "__main__":
+    if userkey == "":
+        st.write('Keyword Cannot Be Blank')
+        sys.exit()
+    else:
+        keyword = userkey.lower()
+        keylist = keyword.split(",")
+
+    for keyword in keylist:
+        Keyword.keysearch(keyword)
+
+    for _ in range(600):
+        try:
+            if not Keyword.mylists:
+                st.write('{}: Product Not Found for {}, Will Look Again...'.format(time.strftime("%I:%M:%S"),keyword).title())
+                time.sleep(0.25)
+                Keyword.keysearch(keyword)
+        except Exception as e:
+            st.write('{}: or Webstore Closed'.format(e))
+    st.write('Program Ended')
+    st.write('------------------------------------------------------------------------------------------------------------')
--- Keyword.py
+++ Keyword.py
@@ -3,9 +3,10 @@ import json
 import requests
 import time
 import logging
+import streamlit as st
 
 
-def keysearch(key):
+def keysearch(keyword):
     logging.basicConfig(level=logging.INFO, filename='Supreme_Log.log', filemode='a',
                         format = " %(asctime)s %(message)s",
                         datefmt="%m/%d/%Y %I:%M:%S %p ")
@@ -22,7 +23,7 @@ def keysearch(key):
         for x in categories.split():
             for result in data['products_and_categories']['{}'.format(x)]:
                 if keyword in result['name'].lower():
-                    print('Product Found!')
+                    st.write('Product Found!')
                     name = result['name']
                     id = result['id']
                     if str(id)[0] == '3':
@@ -33,32 +34,13 @@ def keysearch(key):
                     price = '${}'.format(result['price']*.01)
                     link = 'https://www.supremenewyork.com/shop/{}/{}'.format(x, id)
                     mylist.append(id)
-                    print(len(mylist), end=""),
-                    print('.)', end = ""),
-                    print(name,'-',cat, '-', price)
+                    st.write(name,'-',cat, '-', price)
+                    st.write('\n')
                     webbrowser.open(link)
-                    print('Product Found at {} and Opened in {:.2f} Seconds'.format(time.strftime("%I:%M:%S"),time.time()-starttime))
+                    st.write('Product Found at {} and Opened in {:.2f} Seconds'.format(time.strftime("%I:%M:%S"),time.time()-starttime))
                     logging.info('{}: {} Found Using "{}" at {} and Opened in {:.2f} Seconds'.format(region, name, keyword, time.strftime("%I:%M:%S"),time.time()-starttime))
-                    print()
 
 
-if __name__ == "__main__":
-    keyword = input('Enter Keyword(s), Hit Enter When Ready:').lower()
-    keylist = keyword.split(",")
-    print()
 
-    for keyword in keylist:
-        keysearch(keyword)
-
-    for _ in range(600):
-        try:
-            if not mylists:
-                print('{}: Product Not Found for {}, Will Look Again...'.format(time.strftime("%I:%M:%S"),keyword).title())
-                time.sleep(0.25)
-                keysearch(keyword)
-        except Exception as e:
-            print('{}: or Webstore Closed'.format(e))
-    print('Program Ended')
-    print('------------------------------------------------------------------------------------------------------------')
 
 
